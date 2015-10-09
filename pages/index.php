96388e9c9babe47974844a1a145751c9070aa90f
kamleshr <kamleshr@KAMLESHR.mkclindia.local>
2015-10-09 15:40:10 +0530

    pages folder added which holds the php and html pages

---
+++ pages/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <link href="../resources/bootstrap-3.3.5/css/bootstrap.min.css" rel="stylesheet">    
+    <!--[if lt IE 9]>
+      <script src="../resources/iehtml5js/html5shiv.min.js"></script>
+      <script src="../resources/iehtml5js/respond.min.js"></script>
+    <![endif]-->
+    
+  </head>
+  <body>
+      
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-12">
+                <h2>Heading</h2>
+            </div>
+            <div class="col-sm-12">
+                <?php
+                    print("PHP Code goes here....");
+                ?>
+            </div>
+        </div>
+    </div>
+    
+    
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="../resources/jquery/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="../resources/bootstrap-3.3.5/js/bootstrap.min.js"></script>
+  </body>
+</html>
